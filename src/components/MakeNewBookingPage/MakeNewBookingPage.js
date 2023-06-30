dcf493b0923ad41d16769dbd5ebe13a1ee5668a9
rlly92 <reubenlimliangyi@gmail.com>
2023-06-30 18:41:06 +0800

    completed basic get bands available functionality on MakeNewBookingPage.js

--- src/components/MakeNewBookingPage/MakeNewBookingPage.js
+++ src/components/MakeNewBookingPage/MakeNewBookingPage.js
@@ -2,16 +2,20 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-import { Button, Stack, TextField, Typography } from "@mui/material";
+import { Button, Stack, TextField, Typography, MenuItem } from "@mui/material";
 // import { toast } from "react-toastify";
 
 const MakeNewBookingPage = () => {
   const [state, setState] = useState({
-    dateInput: "",
-    timeInput: "",
+    startDateInput: "",
+    endDateInput: "",
+    startTimeInput: "",
+    endTimeInput: "",
     venueInput: "",
     eventNameInput: "",
+    genreInput: "",
   });
+  const [genres, setGenres] = useState([]);
   const navigate = useNavigate();
   const accessToken = localStorage.getItem("accessToken");
   console.log(accessToken);
@@ -47,6 +51,7 @@ const MakeNewBookingPage = () => {
     } else navigate("/homepage");
   }, [accessToken, navigate]);
 
+  // This is the useEffect block to load all the genres for display in the genre input field:
   useEffect(() => {
     const loadAllGenres = async () => {
       try {
@@ -58,7 +63,8 @@ const MakeNewBookingPage = () => {
             },
           }
         );
-        console.log(getAllGenres);
+        console.log(getAllGenres.data);
+        setGenres(getAllGenres.data);
       } catch (error) {
         console.error("Error occurred while loading all genres", error);
       }
@@ -68,34 +74,32 @@ const MakeNewBookingPage = () => {
     }
   }, [accessToken]);
 
-  //logic for "Back to main" button to navigate to default home page for logged in users:
-  const navigateToHomePage = () => {
-    navigate("/");
-  };
-
-  const handleSubmit = (e) => {
+  const handleSubmitAvailability = async (e) => {
     e.preventDefault();
+    // Combine the date and time inputs to create startDateTime and endDateTime
+    const startDateTime = `${state.startDateInput}T${state.startTimeInput}`;
+    const endDateTime = `${state.endDateInput}T${state.endTimeInput}`;
 
-    // toast
-    //   .promise(
-    //     createUserWithEmailAndPassword(
-    //       auth,
-    //       state.emailInput,
-    //       state.passwordInput
-    //     ),
-    //     {
-    //       pending: "Creating an account...",
-    //       success: "Successfully created an account!",
-    //       error: "Oops, something went wrong. Try again!",
-    //     }
-    //   )
-    //   .then(() => {
-    //     toast.success("🐝 Successfully created a new account!");
-    //     setState({ emailInput: "", passwordInput: "" });
-    //   })
-    //   .then(() => {
-    //     navigate("/bookings");
-    //   });
+    console.log(startDateTime);
+    console.log(endDateTime);
+    try {
+      const getBandsAvailability = await axios.post(
+        `${process.env.REACT_APP_BACKEND_URL}/avail/genre/${state.genreInput}`,
+
+        {
+          startDateTime: startDateTime,
+          endDateTime: endDateTime,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      console.log(getBandsAvailability);
+    } catch (error) {
+      console.error("Error occurred while finding available bands:", error);
+    }
   };
 
   const handleChange = (e) => {
@@ -103,39 +107,73 @@ const MakeNewBookingPage = () => {
     console.log(state);
   };
 
+  const handleGenreChange = (e) => {
+    setState({ ...state, genreInput: e.target.value });
+    console.log(state);
+  };
+
   return (
     <Stack alignItems={"center"} justifyContent={"center"} my={5}>
       <Typography variant="h2">
-        Join Opus today to hire the best live musicians for your event.
+        Hire the best live musicians for your event. Fill in the form and check
+        artists' availability.
       </Typography>
       <br />
       <br />
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmitAvailability}>
         <Stack
           alignItems={"center"}
           justifyContent={"center"}
           spacing={2}
           mt={2}
         >
-          <TextField
-            required
-            autoComplete="off"
-            value={state.dateInput}
-            size="small"
-            id="dateInput"
-            type="date"
-            onChange={handleChange}
-          ></TextField>
-          <TextField
-            required
-            autoComplete="off"
-            value={state.timeInput}
-            size="small"
-            id="timeInput"
-            type="time"
-            onChange={handleChange}
-          ></TextField>
+          <div>
+            <TextField
+              required
+              autoComplete="off"
+              value={state.startDateInput}
+              size="small"
+              id="startDateInput"
+              type="date"
+              onChange={handleChange}
+            />
+            <Typography variant="h6" component="span">
+              {" - "}
+            </Typography>
+            <TextField
+              required
+              autoComplete="off"
+              value={state.endDateInput}
+              size="small"
+              id="endDateInput"
+              type="date"
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <TextField
+              required
+              autoComplete="off"
+              value={state.startTimeInput}
+              size="small"
+              id="startTimeInput"
+              type="time"
+              onChange={handleChange}
+            />
+            <Typography variant="h6" component="span">
+              {" - "}
+            </Typography>
+            <TextField
+              required
+              autoComplete="off"
+              value={state.endTimeInput}
+              size="small"
+              id="endTimeInput"
+              type="time"
+              onChange={handleChange}
+            />
+          </div>
           <TextField
             required
             autoComplete="off"
@@ -156,21 +194,29 @@ const MakeNewBookingPage = () => {
             label="Event Name"
             onChange={handleChange}
           ></TextField>
+          <TextField
+            required
+            autoComplete="off"
+            select
+            value={state.genreInput}
+            size="small"
+            id="genreInput"
+            label="Genre"
+            onChange={handleGenreChange}
+            sx={{ width: "200px" }}
+          >
+            {genres.map((genre) => (
+              <MenuItem value={genre.id} key={genre.name}>
+                {genre.name}
+              </MenuItem>
+            ))}
+          </TextField>
           <br />
           <Button type="submit" variant="contained">
             Check Availability
           </Button>
         </Stack>
       </form>
-      <br />
-      <br />
-      <Button
-        variant="contained"
-        color="secondary"
-        onClick={navigateToHomePage}
-      >
-        Back to main
-      </Button>
     </Stack>
   );
 };
