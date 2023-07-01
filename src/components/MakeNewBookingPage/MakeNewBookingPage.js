f1e38531043bd4f9b9413443d536bfaec955593b
rlly92 <reubenlimliangyi@gmail.com>
2023-07-01 22:45:25 +0800

    completed 95% of makenewbookingspage.js, except limiting preferences to 3 bands and sending consolidated data to BE

--- src/components/MakeNewBookingPage/MakeNewBookingPage.js
+++ src/components/MakeNewBookingPage/MakeNewBookingPage.js
@@ -14,6 +14,8 @@ import {
   CardContent,
   CardMedia,
   CardActions,
+  Modal,
+  Paper,
 } from "@mui/material";
 
 import { useTheme } from "@mui/material/styles";
@@ -29,9 +31,18 @@ const MakeNewBookingPage = () => {
   });
   const [genres, setGenres] = useState([]);
   const [bandsAvailable, setBandsAvailable] = useState([]);
+  const [selectedBandInfo, setSelectedBandInfo] = useState(null);
+  const [selectedBands, setSelectedBands] = useState([]);
+  const [isViewInfoModalOpen, setIsViewInfoModalOpen] = useState(false);
+  const [isContactMe, setIsContactMe] = useState("");
+  const [isReviewing, setIsReviewing] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
   const navigate = useNavigate();
   const accessToken = localStorage.getItem("accessToken");
   console.log(accessToken);
+  const userId = localStorage.getItem("userId");
+  console.log("userId:", userId);
 
   // For Mobile Responsive View, these const are used in the return block later:
   const theme = useTheme();
@@ -121,6 +132,56 @@ const MakeNewBookingPage = () => {
     }
   };
 
+  // Logic for when user selects their top 3 bands in order of preference:
+  const handleSelectBand = (band) => {
+    setSelectedBands((selectedBands) => {
+      // Check if the band is already selected and what their corresponding index number is wihtin the selectedBands state array:
+      const bandIndex = selectedBands.findIndex(
+        (selectedBand) => selectedBand.id === band.id
+      );
+      // If there is a match, i.e. if the bandIndex for this particular band DOES exist because .findIndex returns a positive number or 0:
+      if (bandIndex !== -1) {
+        // If the band is already selected, remove it from the selected bands (this is under the assumption that the user is clicking the SAME button to deselect band):
+        const updatedSelectedBands = [...selectedBands];
+        updatedSelectedBands.splice(bandIndex, 1);
+        return updateRankings(updatedSelectedBands); // Update rankings after removing the band
+      } else {
+        // If the band is not yet selected (i.e. user has clicked select on a band that before the click was not selected), add it to the selected bands with a ranking
+        const ranking = selectedBands.length + 1; // Add 1 to the selectedBands.length since on the first 'select', rankings have to start from 1 and not 0 like in a normal array.
+        const updatedSelectedBands = [...selectedBands, { ...band, ranking }];
+        console.log(updatedSelectedBands);
+        return updateRankings(updatedSelectedBands); // Update rankings after adding the band
+      }
+    });
+  };
+  // Logic for SORTING the bands in 1.)ascending ranking number order and 2.)in consecutively increasing ranking number order; ensuring no ranking number is skipped/left out:
+  const updateRankings = (bands) => {
+    // Sort the bands based on their rankings in numerically ascending order (regardless of if the current rankings all appear in consecutive order, i.e. some ranking numbers MIGHT be missing)
+    const sortedBands = bands.sort((a, b) => a.ranking - b.ranking);
+
+    // Update the rankings of the bands AFTER sorting to ensure that all the corresponding rankings appear in CONSECUTIVE ascending order, i.e. no ranking number has been skipped or is missing:
+    const updatedBands = sortedBands.map((band, index) => ({
+      ...band,
+      ranking: index + 1,
+    }));
+    console.log(updatedBands);
+    return updatedBands;
+  };
+
+  // Logic for when user hits "Review" Button:
+  const handleReview = (e) => {
+    e.preventDefault();
+    setIsReviewing(true);
+    setIsEditing(false);
+  };
+  // Logic for when user hits "Edit" Button:
+  const handleEdit = (e) => {
+    e.preventDefault();
+    setIsReviewing(false);
+    setIsEditing(true);
+  };
+
+  // ALL THE FORM HANDLE CHANGES:
   const handleChange = (e) => {
     setState({ ...state, [e.target.id]: e.target.value });
     console.log(state);
@@ -129,6 +190,22 @@ const MakeNewBookingPage = () => {
   const handleGenreChange = (e) => {
     setState({ ...state, genreInput: e.target.value });
     console.log(state);
+  };
+
+  const handleBookingOptionChange = (e) => {
+    setIsContactMe(e.target.value);
+  };
+  //
+  //
+
+  console.log("isContactMe:", isContactMe);
+  // THIS IS FOR HANDLING THE OPENING AND CLOSING OF THE VIEW INFO MODAL:
+  const handleOpenViewInfoModal = (band) => {
+    setSelectedBandInfo(band);
+    setIsViewInfoModalOpen(true);
+  };
+  const handleCloseViewInfoModal = () => {
+    setIsViewInfoModalOpen(false);
   };
 
   return (
@@ -158,6 +235,7 @@ const MakeNewBookingPage = () => {
                 label="Start Date & Time"
                 InputLabelProps={{ shrink: true }}
                 onChange={handleChange}
+                disabled={isReviewing && !isEditing}
                 sx={{
                   width: isMobile ? "100%" : "auto", // Adjust the width based on screen size
                 }}
@@ -175,6 +253,7 @@ const MakeNewBookingPage = () => {
                 label="End Date & Time"
                 InputLabelProps={{ shrink: true }}
                 onChange={handleChange}
+                disabled={isReviewing && !isEditing}
                 sx={{
                   width: isMobile ? "100%" : "auto", // Adjust the width based on screen size
                 }}
@@ -190,6 +269,7 @@ const MakeNewBookingPage = () => {
               type="venue"
               label="Venue"
               onChange={handleChange}
+              disabled={isReviewing && !isEditing}
             ></TextField>
             <TextField
               required
@@ -200,6 +280,7 @@ const MakeNewBookingPage = () => {
               type="eventname"
               label="Event Name"
               onChange={handleChange}
+              disabled={isReviewing && !isEditing}
             ></TextField>
             <TextField
               required
@@ -210,6 +291,7 @@ const MakeNewBookingPage = () => {
               id="genreInput"
               label="Genre"
               onChange={handleGenreChange}
+              disabled={isReviewing && !isEditing}
               sx={{ width: "200px" }}
             >
               {genres.map((genre) => (
@@ -220,51 +302,177 @@ const MakeNewBookingPage = () => {
             </TextField>
             <br />
 
-            <Button type="submit" variant="contained" sx={{ width: "200px" }}>
+            <Button
+              type="submit"
+              variant="contained"
+              sx={{ width: "200px" }}
+              disabled={isReviewing && !isEditing}
+            >
               Check Availability
             </Button>
           </Stack>
         </form>
         {bandsAvailable.length > 0 && (
           <Grid container spacing={2} mt={4}>
-            {bandsAvailable.map((band) => (
-              <Grid item xs={12} sm={6} md={4} key={band.id}>
-                <Card
-                  sx={{
-                    height: "100%",
+            {bandsAvailable.map((band) => {
+              // Check if the band is already selected
+              const isSelected = selectedBands.some(
+                (selectedBand) => selectedBand.id === band.id
+              );
+              // Find the band's ranking if it's already selected
+              const ranking = selectedBands.find(
+                (selectedBand) => selectedBand.id === band.id
+              )?.ranking;
 
-                    display: "flex",
-                    flexDirection: "column",
-                  }}
-                >
-                  <CardMedia
-                    component="img"
+              return (
+                <Grid item xs={12} sm={6} md={4} key={band.id}>
+                  <Card
                     sx={{
                       height: "100%",
+                      display: "flex",
+                      flexDirection: "column",
                     }}
-                    image={band.photoUrl}
-                    alt={band.name}
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography variant="h6">{band.name}</Typography>
-                  </CardContent>
-                  <CardActions sx={{ marginTop: "auto" }}>
-                    <Stack
-                      direction="column"
-                      spacing={1}
-                      sx={{ width: "100%" }}
+                  >
+                    <CardMedia
+                      component="img"
+                      sx={{
+                        height: "100%",
+                      }}
+                      image={band.photoUrl}
+                      alt={band.name}
+                    />
+                    <CardContent sx={{ flexGrow: 1 }}>
+                      <Typography variant="h6">{band.name}</Typography>
+                    </CardContent>
+                    <CardActions sx={{ marginTop: "auto" }}>
+                      <Stack
+                        direction="column"
+                        spacing={1}
+                        sx={{ width: "100%" }}
+                      >
+                        <Button
+                          variant="contained"
+                          fullWidth
+                          onClick={() => handleOpenViewInfoModal(band)}
+                        >
+                          View Info
+                        </Button>
+
+                        {/* View Info Modal */}
+                        <Modal
+                          open={isViewInfoModalOpen}
+                          onClose={handleCloseViewInfoModal}
+                        >
+                          <Paper
+                            sx={{
+                              p: 2,
+                              maxWidth: "90vw",
+                              width: 400,
+                              margin: "0 auto",
+                              mt: 10,
+                            }}
+                          >
+                            {selectedBandInfo && (
+                              <>
+                                <Typography variant="h6" sx={{ mb: 2 }}>
+                                  {selectedBandInfo.name}
+                                </Typography>
+                                <CardMedia
+                                  component="img"
+                                  sx={{
+                                    height: 300,
+                                    objectFit: "cover",
+                                    mb: 2,
+                                  }}
+                                  image={selectedBandInfo.photoUrl}
+                                  alt={selectedBandInfo.name}
+                                />
+                                <Typography variant="body1" sx={{ mb: 2 }}>
+                                  {selectedBandInfo.description}
+                                </Typography>
+                                <Typography variant="body1" sx={{ mb: 2 }}>
+                                  Rate: ${selectedBandInfo.gigRate}
+                                </Typography>
+                              </>
+                            )}
+                          </Paper>
+                        </Modal>
+                        <Button
+                          variant="contained"
+                          fullWidth
+                          onClick={() => handleSelectBand(band)}
+                          disabled={isReviewing && !isEditing}
+                        >
+                          {isSelected ? "Deselect" : "Select"}
+                        </Button>
+                        {isSelected && (
+                          <Typography
+                            variant="body2"
+                            sx={{ textAlign: "right" }}
+                          >
+                            Preference: {ranking}
+                          </Typography>
+                        )}
+                      </Stack>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              );
+            })}
+            <form>
+              <Stack alignItems="center" justifyContent="center" mt={4}>
+                <Typography variant="subtitle1" sx={{ textAlign: "center" }}>
+                  In the event that none of your chosen musicians are available,
+                  please select your preference:
+                </Typography>
+                <Stack
+                  direction={isMobile ? "column" : "row"}
+                  alignItems={isMobile ? "center" : "flex-start"}
+                  justifyContent="center"
+                  spacing={2}
+                  mt={4}
+                >
+                  <TextField
+                    required
+                    autoComplete="off"
+                    select
+                    value={isContactMe}
+                    size="small"
+                    id="bookingOption"
+                    label="Booking Option"
+                    onChange={handleBookingOptionChange}
+                    disabled={isReviewing && !isEditing}
+                    sx={{ width: isMobile ? "100%" : "200px" }}
+                  >
+                    <MenuItem value="true">Contact Me</MenuItem>
+                    <MenuItem value="false">Remove My Booking</MenuItem>
+                  </TextField>
+                  <Button
+                    variant="contained"
+                    sx={{
+                      width: "200px",
+                      backgroundColor: isReviewing ? "green" : "red",
+                    }}
+                    onClick={isReviewing ? handleEdit : handleReview}
+                    disableElevation
+                  >
+                    {isReviewing ? "Edit" : "Review"}
+                  </Button>
+                  {isReviewing && (
+                    <Button
+                      variant="contained"
+                      sx={{
+                        width: "200px",
+                        backgroundColor: "red",
+                      }}
+                      // onClick
                     >
-                      <Button variant="contained" fullWidth>
-                        View Info
-                      </Button>
-                      <Button variant="contained" fullWidth>
-                        Select
-                      </Button>
-                    </Stack>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
+                      Submit Booking
+                    </Button>
+                  )}
+                </Stack>
+              </Stack>
+            </form>
           </Grid>
         )}
       </Stack>
