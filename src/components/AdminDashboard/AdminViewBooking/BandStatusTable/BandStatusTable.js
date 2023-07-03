5d6baf7c7a44feb9ab141b7f9cb4ccbd301245eb
hengmhs <hengmhs@gmail.com>
2023-07-03 18:53:39 +0800

    Clean up BandStatusTable

--- src/components/AdminDashboard/AdminViewBooking/BandStatusTable/BandStatusTable.js
+++ src/components/AdminDashboard/AdminViewBooking/BandStatusTable/BandStatusTable.js
@@ -1,15 +1,16 @@
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import StatusSelect from "./StatusSelect";
-
-import { Box } from "@mui/system";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Box,
+} from "@mui/material";
 
 import { useEffect, useState } from "react";
+
+import StatusSelect from "./StatusSelect";
 
 const BandStatusTable = ({ bands, setTempBody }) => {
   const [rankedBands, setRankedBands] = useState([]);
