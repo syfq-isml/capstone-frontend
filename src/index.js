28477a0cabb650f473fb85d3d0d7d147ad868ab1
Syafiq Ismail <macamyesjer@gmail.com>
2023-07-04 00:31:02 +0800

    add styling to homepage

--- src/index.js
+++ src/index.js
@@ -2,6 +2,56 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import {
+  CssBaseline,
+  ThemeProvider,
+  createTheme,
+  responsiveFontSizes,
+} from "@mui/material";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+
+let theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#00e676",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+    text: {
+      primary: "#eeeeee",
+    },
+  },
+  typography: {
+    fontFamily: ["Inter", "sans-serif"].join(","),
+    h1: {
+      fontWeight: 700,
+    },
+    h2: {
+      fontWeight: 700,
+    },
+    h3: {
+      fontWeight: 700,
+    },
+    h4: {
+      fontWeight: 700,
+    },
+    h5: {
+      fontWeight: 700,
+    },
+    h6: {
+      fontWeight: 700,
+    },
+  },
+});
+
+theme = responsiveFontSizes(theme);
+
+root.render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>
+);
